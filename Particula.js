212652eca60b7662884d5f17111fab9c9df702ba
JimmyCauja <jimmycauja@gmail.com>
2023-11-06 16:20:27 -0500

    Se relaizon el sistem de particulas y se le dio color de fondo

--- Particula.js
+++ Particula.js
@@ -1,9 +1,41 @@
-class Particula{
-    constructor() { }
-    
-    actualizar() { }
-    
-    mostrar() {
-        circle(width / 2, 30, 40);
-    }
+class Particula {
+	constructor() {
+		this.tamanoInicial = random(10, 50);
+		this.tamano = this.tamanoInicial;
+		this.estaMuerta = false;
+		this.tiempoDeVidaInicial = Math.ceil(random(10, 80));
+		this.tiempoDeVida = this.tiempoDeVidaInicial;
+
+		this.pos = createVector(mouseX, mouseY);
+
+		this.vel = createVector(0, random(3, 5));
+		this.vel.rotate(random(360));
+
+		this.g = createVector(0, 0.1);
+	}
+	// Método ==> method update
+	actualizar() {
+		this.vel.add(this.g);
+		this.pos.add(this.vel);
+
+		this.tamano = map(
+			this.tiempoDeVida,
+			0,
+			this.tiempoDeVidaInicial,
+			0,
+			this.tamanoInicial
+		);
+
+		this.tiempoDeVida -= 1;
+
+		if (this.tiempoDeVida <= 0) {
+			this.estaMuerta = true;
+			print('ups, me morí!');
+			circle(this.pos.x, this.pos.y, this.tamanoInicial);
+		}
+	}
+	// Método ==> method display
+	mostrar() {
+		circle(this.pos.x, this.pos.y, this.tamano);
+	}
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
-let p;
+let sistema = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  p = new Particula();
+	createCanvas(windowWidth, windowHeight);
+	angleMode(DEGREES);
 }
 
 function draw() {
-  p.actualizar();
-  p.mostrar();
+	background(255, 50,100);
+
+	let p = new Particula();
+	sistema.push(p);
+
+	for (let i = 0; i < sistema.length; i++) {
+		if (sistema[i].estaMuerta) {
+			sistema.splice(i, 1);
+		}
+	}
+
+	for (let i = 0; i < sistema.length; i++) {
+		sistema[i].actualizar();
+		sistema[i].mostrar();
+	}
+
+	print(sistema.length);
 }
